9841cbdd6d83d2151cd92db4149693b83c639bae
leatep <leateps@gmail.com>
2022-04-07 01:22:09 +0400

    alpha version, fully refactored, released 0.0.1

--- main.go
+++ main.go
@@ -11,46 +11,57 @@ import (
 // 2. [wait mode], wait for signal 2, info about unit, or what to do
 // 3. start independent loop to listen to server, for change unit info or shutdown
 // 4. main loop is unit operation, sending info req by server to operate and manage unit
-func main() {
-	fmt.Println("asd")
+type connection struct {
+	server *server.Client
 }
-func NewClient() {
-	s := server.NewClient()
-	res := server.Resources{map[string]int64{}}
+var res  server.Resources
+var conn connection
+func main() {
+	res.Materials = map[string]int64{}
+	fmt.Println("Client Started")
+	
+	conn.server = server.NewClient()
+	go conn.Mining()
+	conn.ServerConn()
+}
+func (c *connection) Mining() {
 	sleep := time.Second / 10
 
-	for ;s.AboutClient.Running ;time.Sleep(sleep) {
+	for ;c.server.AboutClient.Running ;time.Sleep(sleep) {
 		res.Materials["Ore"] += 1
+
+
 		if res.Materials["Ore"] > 10 {
-			err := s.Send.Encode(&server.MsgFormat{
-				MsgCode: 6, 
-				Resources: res})
+			err := c.server.Send.Encode(
+				&server.MsgFormat{
+					MsgCode: 6, 
+					Resources: res})
 			if err != nil {
 				log.Printf("Can't sent msg: %v\n", err)
-				s.Conn.Close()
-				s.AboutClient.Running = false
-				break
+				c.server.Conn.Close()
+				c.server.AboutClient.Running = false
+				return
 			}
-			res = server.Resources{map[string]int64{}}
+			res.Materials = map[string]int64{}
 		}
 	}
 }
-func ServerConn(s *server.Client) {
-	sleep := time.Second
-	for ;;time.Sleep(sleep) {
-		msg := &server.MsgFormat{}
-		err :=s.Receive.Decode(msg)
-		if err != nil {
+func (c *connection) ServerConn() {
+	var err   error
+	var msg   server.MsgFormat
+	for ;c.server.AboutClient.Running; {
+		msg    = server.MsgFormat{}
+		if err = c.server.Receive.Decode(&msg); err != nil {
 			log.Printf("[Error in receiving msg]: %v", err)
-			s.Conn.Close()
-			s.AboutClient.Running = false
+			c.server.Conn.Close()
+			c.server.AboutClient.Running = false
 		}
 		switch msg.MsgCode {
 		case 1: // ping, saying that server is still alive
 		case 2: // get info about the server
 		case 3: // signal to change settings to...
 		case 4: // signal to shutdown
-			log.Printf("Signal to shutdown at %v\n", time.Now().UTC())
+			log.Fatalf("Signal to shutdown at %v\n", time.Now().UTC())
 		case 5: // signal to reload
 		default:
 			log.Println("0, something wrong")
--- server.go
+++ server.go
@@ -1,55 +1,85 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
-	"time"
 	"server"
 )
-
+type connection struct {
+	server *server.Server
+}
+var i int64  				// client connection number
+var res server.Resources	// data client sending to process
+var pool connection			
 func main() {
-	s 	:= server.NewServer()
-	go s.AcceptConn()
-
+	res.Materials = map[string]int64{}
+	pool.server   = server.NewServer()
 	fmt.Println("server ready")
 
-	res := server.Resources{map[string]int64{}}
-	go receivingLoop(s, &res)
-	time.Sleep(time.Second * 100)
+	for ;;i++{
+		pool.AcceptConn(i)
+	}
+}
+func (c *connection) AcceptConn(i int64) {
+	conn, err := c.server.Listener.Accept() // listen for clients
+	if err != nil {
+		log.Printf("[failed to connect]: %v\n", err)
+	}
+	fmt.Printf("connected [%v]: %v\n", i, conn)
 
-	// sql handler, update and send clients everything that is needed
+	c.server.ClientConn[i] = &server.Client{
+		Conn: 	 conn, 
+		Receive: gob.NewDecoder(conn), 
+		Send: 	 gob.NewEncoder(conn),
+	}
+	go c.ManageConnection(i)
 
 }
 
-func receivingLoop(s *server.Server, res *server.Resources) {
-	sleep := time.Second / 10
-	for ;;time.Sleep(sleep) {
-		for i, v := range s.ClientConn {
-			msg  := server.MsgFormat{}
-			err  := v.Receive.Decode(&msg)
-			if err != nil {
-				log.Printf("%v [err]: %v\n", i, err)     // well would be to put client identifiers like containerId and stuff
-				v.Conn.Close()
-				delete(s.ClientConn, i)
-				continue
+func (c *connection) ManageConnection(i int64) {
+	var count int64 
+	var err   error
+	var msg   server.MsgFormat
+	client    := c.server.ClientConn[i]
+	for {
+		msg    = server.MsgFormat{}
+		if err = client.Receive.Decode(&msg); err != nil {
+			log.Printf("%v [err]: %v\n", i, err)     // well would be to put client identifiers like containerId and stuff
+			client.Conn.Close()
+			delete(c.server.ClientConn, i)
+			return
+		}
+		switch msg.MsgCode {
+		case 1: // get ping that client is active
+		case 2: // get info about client
+			client.AboutClient = &msg.CInfo
+		case 3: // something changed in client
+		case 4: // client shutting down
+			client.Conn.Close()
+			delete(c.server.ClientConn, i)
+			return
+		case 5: // client reloading
+		case 6: // update resources
+			for i, k := range msg.Resources.Materials {
+				res.Materials[i] += k
+				fmt.Println(res.Materials)
 			}
-			switch msg.MsgCode {
-			case 1: // get ping that client is active
-			case 2: // get info about client
-				v.AboutClient = &msg.CInfo
-			case 3: // something changed in client
-			case 4: // client shutting down
-				v.Conn.Close()
-				delete(s.ClientConn, i)
-			case 5: // client reloading
-			case 6: // update resources
-				for i, k := range msg.Resources.Materials {
-					res.Materials[i] += k
-					fmt.Println(res.Materials)
-				}
-			default:
-				fmt.Println("0, something wrong")
+		
+			if count == 2 {
+				client.Send.Encode(server.MsgFormat{MsgCode: 4})
+				client.Conn.Close()
+				delete(c.server.ClientConn, i)
+				return
 			}
+			count++
+		default:
+			fmt.Println("0, something wrong")
 		}
 	}
 }
+
+// c.Send.Encode
+// c.Receive.Decode
+// c.Conn.Close 
+// work just fine, but if needed to be put in interface, then it will be necessary? 
